c7a348f502dec2eab16d55156b0776d6dfd9b154
Haba <mihajlukristina@gmail.com>
2026-05-25 18:48:44 +0300

    add auth route protection

--- proxy.ts
+++ proxy.ts
@@ -2,9 +2,55 @@ import { NextResponse } from "next/server";
 
 import type { NextRequest } from "next/server";
 
-export function proxy(
+export async function proxy(
   request: NextRequest
 ) {
+
+  const token =
+    request.cookies.get(
+      "sb-access-token"
+    );
+
+  const isAuthPage =
+    request.nextUrl.pathname.startsWith(
+      "/login"
+    ) ||
+    request.nextUrl.pathname.startsWith(
+      "/signup"
+    );
+
+  const isDashboard =
+    request.nextUrl.pathname.startsWith(
+      "/dashboard"
+    );
+
+  if (
+    !token &&
+    isDashboard
+  ) {
+
+    return NextResponse.redirect(
+      new URL(
+        "/login",
+        request.url
+      )
+    );
+
+  }
+
+  if (
+    token &&
+    isAuthPage
+  ) {
+
+    return NextResponse.redirect(
+      new URL(
+        "/dashboard",
+        request.url
+      )
+    );
+
+  }
 
   return NextResponse.next();
 
